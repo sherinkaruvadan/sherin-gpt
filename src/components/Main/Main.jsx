506a8a4b10fc7e2cfcb7fa336462e2d945170197
sherinanooj <sherin.karuvadan@gmail.com>
2025-04-24 12:57:46 -0400

    update main section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,32 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="user-icon" className="main__image" />
       </div>
+      <div className="main__container">
+        <div className="main__greet">
+          <p>
+            <span>Hello, Dev</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="main__cards">
+          <div className="main__card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="compass-icon" />
+          </div>
+          <div className="main__card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="compass-icon" />
+          </div>
+          <div className="main__card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="compass-icon" />
+          </div>
+          <div className="main__card">
+            <p>Improve the readability of following code</p>
+            <img src={assets.code_icon} alt="compass-icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
